ff88a27faed685494149615a0d72452548cda47a
deepcloudlabs <deepcloudlabs@gmail.com>
2021-05-25 11:29:29 +0300

    initial commit

--- mastermind/src/app/mastermind.service.ts
+++ mastermind/src/app/mastermind.service.ts
@@ -13,19 +13,23 @@ export class MastermindService {
   constructor() {
   }
 
-  play() : void {
-      this.mastermindGame.tries++;
-      if (this.mastermindGame.guess == this.secret){
-         this.mastermindGame.gameLevel++;
-         this.mastermindGame.reset();
-         this.secret = this.createSecret(this.mastermindGame.gameLevel)
+  play(): void {
+    this.mastermindGame.tries++;
+    if (this.mastermindGame.guess == this.secret) {
+      if (this.mastermindGame.gameLevel == 10) {
+        //TODO: Player wins!
       } else {
-        if (this.mastermindGame.tries > 10){
-          //TODO: Player loses!
-        } else {
-          this.mastermindGame.moves.push(this.createMove(this.mastermindGame.guess,this.secret));
-        }
+        this.mastermindGame.gameLevel++
+        this.mastermindGame.reset();
+        this.secret = this.createSecret(this.mastermindGame.gameLevel)
       }
+    } else {
+      if (this.mastermindGame.tries > 10) {
+        //TODO: Player loses!
+      } else {
+        this.mastermindGame.moves.push(this.createMove(this.mastermindGame.guess, this.secret));
+      }
+    }
   }
 
   private createSecret(level: number): number {
@@ -43,30 +47,30 @@ export class MastermindService {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  private createMove(guess: number, secret: number) : Move {
+  private createMove(guess: number, secret: number): Move {
     let evaluation = "";
     let perfectMatch = 0;
     let partialMatch = 0;
     const guessAsString = guess.toString();
     const secretAsString = secret.toString();
-    for (let i=0;i<guessAsString.length;++i){
+    for (let i = 0; i < guessAsString.length; ++i) {
       const g = guessAsString.charAt(i);
-      for (let j=0;j<secretAsString.length;++j){
+      for (let j = 0; j < secretAsString.length; ++j) {
         const s = secretAsString.charAt(j);
-        if (g==s){
-            if (i==j)
-               perfectMatch++;
-            else
-              partialMatch++;
+        if (g == s) {
+          if (i == j)
+            perfectMatch++;
+          else
+            partialMatch++;
         }
       }
     }
-    if (perfectMatch==0 && partialMatch==0)
-      return new Move(guess,"No Match",perfectMatch,partialMatch);
-    if (partialMatch>0)
-       evaluation += "-" + partialMatch;
-    if (perfectMatch>0)
-       evaluation += "+" + perfectMatch;
-    return new Move(guess,evaluation,perfectMatch,partialMatch);
+    if (perfectMatch == 0 && partialMatch == 0)
+      return new Move(guess, "No Match", perfectMatch, partialMatch);
+    if (partialMatch > 0)
+      evaluation += "-" + partialMatch;
+    if (perfectMatch > 0)
+      evaluation += "+" + perfectMatch;
+    return new Move(guess, evaluation, perfectMatch, partialMatch);
   }
 }
