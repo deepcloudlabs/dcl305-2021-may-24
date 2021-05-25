d8ea225946ae9f3a30abc83986ad7cb9fb43fa0e
deepcloudlabs <deepcloudlabs@gmail.com>
2021-05-25 12:23:25 +0300

    initial commit

--- mastermind/src/app/app.module.ts
+++ mastermind/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { MoveComponent } from './move/move.component';
+import { HistoryComponent } from './history/history.component';
+import { EvaluationComponent } from './evaluation/evaluation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoveComponent,
+    HistoryComponent,
+    EvaluationComponent
   ],
   imports: [
     BrowserModule,
